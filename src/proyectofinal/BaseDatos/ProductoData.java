2c385c614561e2b405808995f862bc44ef0eba8a
PerZZi <emiliano.estebandiaz@gmail.com>
2023-10-02 14:23:24 -0300

    Metodo agregar producto

--- src/proyectofinal/BaseDatos/ProductoData.java
+++ src/proyectofinal/BaseDatos/ProductoData.java
@@ -5,10 +5,51 @@
  */
 package proyectofinal.BaseDatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import proyectofinal.Tablas.Producto;
+
 /**
  *
  * @author Usuario
  */
 public class ProductoData {
+    
+    private Connection con = null;
+    private Set<Producto> productos=new HashSet<>(); 
+
+    public ProductoData() {
+        con = Conexion.getConnection();
+    }
+    
+    public void agregarProducto(Producto producto){
+        
+        String sql= "INSERT INTO producto (nombre, stock, precio) VALUES (?, ?, ?)";
+        try{
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, producto.getNombre());
+            ps.setInt(2, producto.getStock());
+            ps.setDouble(3, producto.getPrecio());
+            ps.executeUpdate();
+            ResultSet rs= ps.getGeneratedKeys();
+            if(rs.next()){
+                producto.setIdCodigo(1);
+                JOptionPane.showMessageDialog(null, "Se agrego un producto");
+            }
+            ps.close();
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla producto");
+        }
+    }
     
 }
