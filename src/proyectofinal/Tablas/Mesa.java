1a0a5dc6e309067eca2f381449c5e1526933ab90
nohaa22 <141742895+nohaa22@users.noreply.github.com>
2023-10-02 12:36:45 -0400

    tablaMesa2

--- src/proyectofinal/Tablas/Mesa.java
+++ src/proyectofinal/Tablas/Mesa.java
@@ -1,14 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectofinal.Tablas;
 
-/**
- *
- * @author Noelia
- */
+import java.time.LocalDate;
+
+
 public class Mesa {
+    
+    private int idMesa;
+    private String nombreCliente;
+    private int dni;
+    private LocalDate fecha;
+    private LocalDate hora;
+    private int estado;
+
+    public Mesa() {
+    }
+
+    public Mesa(int idMesa, String nombreCliente, int dni, LocalDate fecha, LocalDate hora, int estado) {
+        this.idMesa = idMesa;
+        this.nombreCliente = nombreCliente;
+        this.dni = dni;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = estado;
+    }
+
+    public int getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(int idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalDate getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalDate hora) {
+        this.hora = hora;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return  idMesa + "," + nombreCliente + "," + dni + "," + fecha + "," + hora + ", " + estado ;
+    }
+
+    
+    
+    
     
 }
