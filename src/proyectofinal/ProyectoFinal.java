d580fc03029de63ec653a55f761d5a3c5c24f024
PerZZi <emiliano.estebandiaz@gmail.com>
2023-10-03 12:02:43 -0300

    Clase producto
    se agrego otro constructor sin idcodigo

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -22,18 +22,18 @@ public class ProyectoFinal {
     public static void main(String[] args) {
 
         Connection con = Conexion.getConnection();
-        Producto pro1 = new Producto();
-        pro1.setNombre("tallarines");
-        pro1.setStock(20);
-        pro1.setPrecio(500.0);
-        ProductoData proData = new ProductoData();
-        proData.agregarProducto(pro1);
-        
-        Producto pro2 = new Producto();
-        pro2.setNombre("tallarines");
-        pro2.setStock(20);
-        pro2.setPrecio(500.0);
-        ProductoData pro2Data = new ProductoData();
-        pro2Data.agregarProducto(pro2);
+//        Producto pro1 = new Producto();
+//        pro1.setNombre("tallarines");
+//        pro1.setStock(20);
+//        pro1.setPrecio(500.0);
+//        ProductoData proData = new ProductoData();
+//        proData.agregarProducto(pro1);
+//        
+//        Producto pro2 = new Producto();
+//        pro2.setNombre("tallarines");
+//        pro2.setStock(20);
+//        pro2.setPrecio(500.0);
+//        ProductoData pro2Data = new ProductoData();
+//        pro2Data.agregarProducto(pro2);
     }
 }
